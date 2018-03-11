5e70dad2404b8adf2094878f6c4adc045525aa58
Raeceph <cephy88@gmail.com>
2018-03-11 11:23:43 +0800

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// Other imports removed for brevity
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
-import { AppComponent } from './app.component';
-
+// Paste in your credentials that you saved earlier
+var firebaseConfig = {
+   apiKey: "AIzaSyB04tVUXI6Qyp9a2yCnBjYMjHwe1z4En3M",
+    authDomain: "firestore-12df8.firebaseapp.com",
+    databaseURL: "https://firestore-12df8.firebaseio.com",
+    projectId: "firestore-12df8",
+    storageBucket: "",
+    messagingSenderId: "824089972118"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule                            // And this
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
